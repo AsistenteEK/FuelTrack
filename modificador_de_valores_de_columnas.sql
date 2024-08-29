c4aabe19b055feed7b6c5b6fcd1f46aef3cd4bda
AsistenteEK <masistenteek@gmail.com>
2024-08-29 12:18:12 -0500

    eliminados errores

--- modificador_de_valores_de_columnas.sql
+++
@@ -1,6 +0,0 @@
--- alter es para modificar la estructura de la tabla ya existente, cómo cambiar el tipo de datos
-ALTER TABLE mantenimiento
-MODIFY cloudfleet VARCHAR(10);
-
--- para ver el formato de caracteres de una tabla específica, puedes usar el comando
-SHOW TABLE STATUS LIKE 'mantenimiento';
